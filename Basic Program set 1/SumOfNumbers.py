90f96ae67f829f91d02834e3d3ddc069958c822e
Nikhil Ranjan <nikhilranjan1213@gmail.com>
2026-02-24 01:53:16 +0530

    Updated Details

---
+++ Basic Program set 1/SumOfNumbers.py
@@ -0,0 +1,7 @@
+A= int (input("Please Enter the first number below: \n"))
+B = int (input("Please Enter the second number below: \n"))
+#  \n is used to move the cursor to the next line after the user input
+
+C=A+B
+
+print("The sum of A and B is ", C)
---
+++ Basic Program set 1/findSquare.py
@@ -0,0 +1,5 @@
+#Take a number from user and print its square
+A = int (input('Enter the number '))
+square= A*A
+print('The square of ', A, 'is', square)
+
