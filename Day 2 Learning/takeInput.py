28b58a194dcd89f4c85ebcade213e0f99119230b
Nikhil Ranjan <nikhilranjan1213@gmail.com>
2026-02-26 00:05:10 +0530

    Day 3

---
+++ Day 2 Learning/takeInput.py
@@ -0,0 +1,3 @@
+a=input("enter the number")
+a=int(a)
+print(a+3)
---
+++ Day 3 Learning/EscapePRINT.py
@@ -0,0 +1,17 @@
+# we will be using some special ESCAPE to print below statements
+
+#-> Hello 'Nikhil', It is nice to meet you 
+
+#-> Help me!, 
+# "please. can anyone help me"
+
+#-> RAM : He said " I will do it", But 
+#-> Tinku : But what!
+
+A= ("Hello \'Nikhil\', it is nice to meet you")
+B=("Help me!, \n\"please, can anyone help me\"")
+C=("RAM : He said \" I will do it\", But \nTinku : But what!")
+
+print(A)
+print(B)
+print(C)
